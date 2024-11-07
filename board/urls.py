9117e3f0e56ccd3923ad03a02850a26a451f833f
H.Lee <ransom722@naver.com>
2024-11-07 21:43:35 +0900

    minor upgrade

--- board/urls.py
+++ board/urls.py
@@ -5,4 +5,6 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.list, name = 'list'),
+    path('read/<int:id>', views.read, name = 'read'),
 ]
